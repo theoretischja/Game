7f4f2d56fd97eccc814698349f3657f919052fe7
Em-Steen <emma.c_steen@me.com>
2023-01-31 19:42:26 +0000

    initial commit

---
+++ game.js
@@ -0,0 +1,16 @@
+//GAME
+//outline: memory game with 8 cards that needs to you match anime characters from the same show
+//features: shows randomised anime gif when you win; displays result; displays highscore (for as long as you don't exit the game)
+
+//steps:
+//understanding the problem: what is it asking you to do? what’s the input and the output?
+// -> create a game that lets a user interact with the website, and which takes their input, evaluates it, stores it, and displays results
+// -> input: user clicks / output: game results
+//write pseudocode: break problem down into key steps; use JavaScript comments
+// -> write html for rough framework according to wireframe
+// -> write css for rough framework according to wireframe (very rough!)
+// -> break jss functionality down into small functions
+// -> when functionality works, refine html and css
+//write JS for each step following the pseudocode (console.log() to check code at each step)
+//refactor: how can you change your code to make it more efficient? how might you solve the problem differently?
+
